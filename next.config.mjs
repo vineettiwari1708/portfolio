4c5585e0c0413134ca42988a1f7f22284f7fcaa1
unknown <vineettiwari1708@gmail.com>
2026-01-19 01:00:34 +0530

    netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { experimental: {
+    useDeploymentId: true,
+    // Optionally, use with Server Actions
+    useDeploymentIdServerActions: true,
+  },};
 
 export default nextConfig;
